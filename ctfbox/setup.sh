e1f32500584adf3196617c2a98ac8388c551bdf3
boogy <theboogymaster@gmail.com>
2016-02-25 13:51:27 +0100

    update

--- ctfbox/setup.sh
+++ ctfbox/setup.sh
@@ -13,7 +13,7 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get -y -q install git sudo \
         python2.7 python-pip python-dev python3-pip python3-dev \
         tmux gdb gdb-multiarch foremost ipython stow build-essential \
-        ltrace strace socat tcpdump john hydra && \
+        ltrace strace socat tcpdump john hydra curl wget vim
 
 ## QEMU with MIPS/ARM - http://reverseengineering.stackexchange.com/questions/8829/cross-debugging-for-mips-elf-with-qemu-toolchain
 apt-get -y -q install qemu qemu-user qemu-user-static 'binfmt*' libc6-armhf-armel-cross debian-keyring debian-archive-keyring emdebian-archive-keyring
